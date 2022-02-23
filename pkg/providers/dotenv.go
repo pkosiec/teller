0e2b0071c7386eee8947e2512af95edfbdcdb52c
Pawel Kosiec <pawel.kosiec@gmail.com>
2022-02-23 15:18:50 +0100

    Refactor dotenv

--- pkg/providers/dotenv.go
+++ pkg/providers/dotenv.go
@@ -19,12 +19,18 @@ const (
 type DotEnvClient interface {
 	Read(p string) (map[string]string, error)
 	Write(p string, kvs map[string]string) error
+	Exists(p string) (bool, error)
+	Delete(p string) error
 }
 type DotEnvReader struct {
 }
 
 func (d *DotEnvReader) Read(p string) (map[string]string, error) {
 	content, err := os.ReadFile(p)
+	if os.IsNotExist(err) {
+		return nil, nil
+	}
+
 	if err != nil {
 		return nil, err
 	}
@@ -36,7 +42,46 @@ func (d *DotEnvReader) Write(p string, kvs map[string]string) error {
 	if err != nil {
 		return err
 	}
+
+	p, err = homedir.Expand(p)
+	if err != nil {
+		return err
+	}
+
+	// ensure all subdirectories exist
+	err = os.MkdirAll(path.Dir(p), dirPerm)
+	if err != nil {
+		return err
+	}
+
 	return os.WriteFile(p, []byte(content), filePerm)
+}
+
+func (d *DotEnvReader) Exists(p string) (bool, error) {
+	p, err := homedir.Expand(p)
+	if err != nil {
+		return false, err
+	}
+
+	_, err = os.Stat(p)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (d *DotEnvReader) Delete(p string) error {
+	p, err := homedir.Expand(p)
+	if err != nil {
+		return err
+	}
+
+	return os.Remove(p)
 }
 
 type Dotenv struct {
@@ -59,37 +104,28 @@ func (a *Dotenv) Put(p core.KeyPath, val string) error {
 }
 
 func (a *Dotenv) PutMapping(kp core.KeyPath, m map[string]string) error {
-	p, err := homedir.Expand(kp.Path)
+	exists, err := a.client.Exists(kp.Path)
 	if err != nil {
 		return err
 	}
 
-	// check if the file does exist
-	_, err = os.Stat(p)
-	switch {
-	case err == nil:
-		// get a fresh copy of a hash
-		var into map[string]string
-		into, err = a.client.Read(p)
-		if err != nil {
-			return err
-		}
-		utils.Merge(m, into)
-		return a.client.Write(p, into)
-	case os.IsNotExist(err):
-		// ensure all subdirectories exist
-		err = os.MkdirAll(path.Dir(p), dirPerm)
-		if err != nil {
-			return err
-		}
-		return a.client.Write(p, m)
-	default:
+	if !exists {
+		return a.client.Write(kp.Path, m)
+	}
+
+	// get a fresh copy of a hash
+	var into map[string]string
+	into, err = a.client.Read(kp.Path)
+	if err != nil {
 		return err
 	}
+
+	utils.Merge(m, into)
+	return a.client.Write(kp.Path, into)
 }
 
 func (a *Dotenv) GetMapping(p core.KeyPath) ([]core.EnvEntry, error) {
-	kvs, err := a.getSecrets(p)
+	kvs, err := a.client.Read(p.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -102,7 +138,7 @@ func (a *Dotenv) GetMapping(p core.KeyPath) ([]core.EnvEntry, error) {
 }
 
 func (a *Dotenv) Get(p core.KeyPath) (*core.EnvEntry, error) {
-	kvs, err := a.getSecrets(p)
+	kvs, err := a.client.Read(p.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -119,7 +155,7 @@ func (a *Dotenv) Get(p core.KeyPath) (*core.EnvEntry, error) {
 }
 
 func (a *Dotenv) Delete(kp core.KeyPath) error {
-	kvs, err := a.getSecrets(kp)
+	kvs, err := a.client.Read(kp.Path)
 	if err != nil {
 		return err
 	}
@@ -128,7 +164,7 @@ func (a *Dotenv) Delete(kp core.KeyPath) error {
 	delete(kvs, k)
 
 	if len(kvs) == 0 {
-		return a.DeleteMapping(kp) // delete whole secret
+		return a.DeleteMapping(kp)
 	}
 
 	p, err := homedir.Expand(kp.Path)
@@ -140,18 +176,15 @@ func (a *Dotenv) Delete(kp core.KeyPath) error {
 }
 
 func (a *Dotenv) DeleteMapping(kp core.KeyPath) error {
-	p, err := homedir.Expand(kp.Path)
+	exists, err := a.client.Exists(kp.Path)
 	if err != nil {
 		return err
 	}
 
-	return os.Remove(p)
-}
-
-func (a *Dotenv) getSecrets(kp core.KeyPath) (map[string]string, error) {
-	p, err := homedir.Expand(kp.Path)
-	if err != nil {
-		return nil, err
+	if !exists {
+		// already deleted
+		return nil
 	}
-	return a.client.Read(p)
+
+	return a.client.Delete(kp.Path)
 }
